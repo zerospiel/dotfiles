6ca2dff694fe6f8d22d0484d376f8b389f8e7e3b
zerospiel <morgoevm@gmail.com>
2022-05-28 16:57:54 +0300

    Update configs

--- vscode/extensions.bash
+++ vscode/extensions.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-code --install-extension 13xforever.language-x86-64-assembly
-code --install-extension adamprzybylski.paradox-syntax
 code --install-extension bmarkovic.haproxy
 code --install-extension bradymholt.pgformatter
 code --install-extension bufbuild.vscode-buf
@@ -10,37 +8,26 @@ code --install-extension dakara.transformer
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
-code --install-extension EditorConfig.EditorConfig
+code --install-extension enkia.tokyo-night
 code --install-extension golang.go
+code --install-extension huntertran.auto-markdown-toc
 code --install-extension jeff-hykin.better-cpp-syntax
 code --install-extension mads-hartmann.bash-ide-vscode
 code --install-extension manuth.markdown-converter
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-dotnettools.csharp
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-mssql.mssql
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.mono-debug
 code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension naco-siren.gradle-language
 code --install-extension rebornix.ruby
-code --install-extension redhat.java
-code --install-extension redhat.vscode-commons
 code --install-extension redhat.vscode-yaml
 code --install-extension shakram02.bash-beautify
 code --install-extension SonarSource.sonarlint-vscode
 code --install-extension sumneko.lua
-code --install-extension tootone.org-mode
-code --install-extension twxs.cmake
 code --install-extension vintharas.learn-vim
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-dependency
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-java-test
-code --install-extension vscjava.vscode-maven
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension vscodevim.vim
 code --install-extension wayou.vscode-todo-highlight
